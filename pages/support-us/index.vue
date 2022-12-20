b26a8a8ead2100eadd3587ccf978ec8a10d3e2fd
Ahmad Fauzi <ahmad.uji1902@gmail.com>
2022-12-20 17:50:38 +0700

    fix: wording

--- pages/support-us/index.vue
+++ pages/support-us/index.vue
@@ -2,36 +2,33 @@
   <Layout
     :with-footer="false"
   >
-    <div class="mb-20">
+    <div class="mt-10 mb-20">
       <div class="w-full">
-        <span class="text-lg text-center">Welcome <b>┌( ಠ‿ಠ)┘</b></span>
+        <span class="text-lg text-center">Hi, thank you for visiting this page! <b>(๑‵●‿●‵๑)</b></span>
 
         <div class="mt-8 text-xs">
-          First of all, thank you for visiting this page <b>(๑‵●‿●‵๑)</b>
+          Welcome to Upy, a community platform that is specifically designed for artists.
           <br><br>
-          This platform is currently developed and maintained by only one person but I consider myself as a 'we' not 'I'. If you enjoyed this platform, please consider donating to the developer, so we can continue to make this platform better and of course pay the bills <b>(｀∀´)</b>
+          Our main goal is not only to provide a portfolio site where you can showcase your work, but also to create a space where artists can interact with each other and collaborate on creative feature.
           <br><br>
-          We never planned to put ads on this site because we really hate ads, it's really annoying and will destroy the experience of this site, so any donations are appreciated and will be used to pay for server costs and other costs.
+          By choosing to support Upy, you are showing your commitment to our platform and helping us continue to grow and improve. We value your input and are always open to hearing your suggestions for new features or improvements. Any funds that we receive through sponsorship will be used to cover our server and other expenses, as well as allow us to hire additional developers to help us work more efficiently and effectively.
           <br><br>
-          We planned to make this platform as a community platform, where people can share their ideas and innovations to make this platform better, 
-          you can request a new feature or some feature changes that comes up to your mind by submitting a request at <nuxt-link :to="'build-together'" class="font-bold underline">developer space</nuxt-link>,
-          then we will review your request and determine if your idea is suitable or possible to be added to this platform or not.
-          <br><br>
-          For anyone who can code or who can help us to make this platform better, please consider contributing to the project. We are planning to make this project as open-source at some point in the future, but for now you can message us on <a class="font-bold underline" href="https://twitter.com/upydotmoe">Twitter</a> if you willing to help our core team member develop this platform.
+          Thank you for considering a supporter of Upy and for supporting the artistic community.<br><br>
+          Much love~ 😘
         </div>
       </div>
 
       <div class="mt-10">
         <span class="text-lg italic">Why should I support Upy?</span>
 
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-3 mt-4">
+        <div class="grid grid-cols-1 gap-3 mt-4 md:grid-cols-3">
           <!-- support the team -->
           <div class="flex flex-col p-6 w-full text-center rounded-md shadow-sm hover:shadow-xl theme-color">
             <Icon :name="'i-fluent-people-checkmark-24-regular'" class="mx-auto text-4xl" />
             <div class="mt-4 text-left">
               <span class="text-base font-bold">Support the team</span>
               <div class="mt-1">
-                A small team (currently one person) runs Upy, your support will help them to continue to make this platform better.
+                A dedicated team of professionals (currently consisting of one individual) operates Upy, and your support will enable them to continually improve and enhance the platform.
               </div>
             </div>
           </div>
@@ -42,13 +39,13 @@
             <div class="mt-4 text-left">
               <span class="text-base font-bold">Server and other bills</span>
               <div class="mt-1">
-                Donation go towards the server and other bills for running the website and keep it alive.
+                Donations are utilized to cover the expenses associated with maintaining and operating the website, including server costs and other necessary bills.
               </div>
             </div>
           </div>
 
           <!-- no ads -->
-          <div class="flex flex-col p-6 w-full text-center rounded-md shadow-sm hover:shadow-xl theme-color">
+          <!-- <div class="flex flex-col p-6 w-full text-center rounded-md shadow-sm hover:shadow-xl theme-color">
             <Icon :name="'i-ion-sad-outline'" class="mx-auto text-4xl" />
             <div class="mt-4 text-left">
               <span class="text-base font-bold">Stay Ad-free</span>
@@ -56,7 +53,7 @@
                 Your donations help keep the platform free from annoying ads and outside sponsors.
               </div>
             </div>
-          </div>
+          </div> -->
 
           <!-- open-source -->
           <!-- <div class="flex flex-col p-6 w-full text-center rounded-md shadow-sm hover:shadow-xl theme-color">
@@ -75,7 +72,7 @@
             <div class="mt-4 text-left">
               <span class="text-base font-bold">Make the platform even better</span>
               <div class="mt-1">
-                By donating to our platform that means it encourages us to add more features to this platform, we are pleased to receive any features you guys requested, because that's means you guys care about this platform and want to make it better.
+                Your donations to our platform serve as encouragement for us to continue adding new features and improving the overall user experience. We welcome any feature requests as they demonstrate your commitment to and appreciation for the platform. Your support enables us to continuously enhance and refine the platform for the benefit of all users.
               </div>
             </div>
           </div>
@@ -85,7 +82,7 @@
       <div class="mt-10">
         <span class="text-lg">Donation options</span>
 
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 mt-4">
+        <div class="grid grid-cols-1 gap-3 mt-4 md:grid-cols-2 lg:grid-cols-5">
           <!-- PayPal -->
           <a :href="'https://paypal.me/noric1902'" target="_blank" class="flex flex-col justify-center p-6 w-full text-center align-middle rounded-md shadow-sm hover:shadow-xl theme-color">
             <img class="mx-auto mb-4 w-14 h-14" src="https://cdn.iconscout.com/icon/free/png-256/paypal-1527455-1298285.png">
@@ -108,6 +105,12 @@
           <a :href="'https://saweria.co/upydotmoe'" target="_blank" class="flex flex-col justify-center p-6 w-full text-center align-middle rounded-md shadow-sm hover:shadow-xl theme-color">
             <img class="mx-auto mb-4 w-14 h-14" src="https://d1fdloi71mui9q.cloudfront.net/vqAU6rLS5ySXzmBdFaaU_saweria%20icon.png">
             <span class="mx-auto text-lg font-bold">saweria</span>
+          </a>
+
+          <!-- github sponsor -->
+          <a :href="'https://github.com/sponsors/uuppyy'" target="_blank" class="flex flex-col justify-center p-6 w-full text-center align-middle rounded-md shadow-sm hover:shadow-xl theme-color">
+            <img class="mx-auto mb-4 w-14 h-14" src="/others/gh-sponsor.png">
+            <span class="mx-auto text-lg font-bold">Github Sponsor</span>
           </a>
         </div>
       </div>
