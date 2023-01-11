3880ff75bc4b4632f37b77d06a3c148fca3e6d50
Ahmad Fauzi <ahmad.uji1902@gmail.com>
2023-01-11 20:23:36 +0700

    chore: update deploy command

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-npm i -g yarn
-yarn install --ignore-engines
+npm install
 
 echo "Building app.."
-yarn build
+npm run build
 
 echo "Restarting PM2.."
-yarn pm2:stop
-yarn pm2:start
+npm run pm2:stop
+npm run pm2:start
 
 echo "Deployed successfully!"
