274d79e70800f12fb2e3a81955c5bd353d88ef6a
Ahmad Fauzi <ahmad.uji1902@gmail.com>
2023-01-20 15:34:43 +0700

    chore: add `autorestart` to pm2 configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,16 @@ module.exports = {
       instances: 'max',
       script: './.output/server/index.mjs',
       watch: false,
+      autorestart: true,
       args: 'start',
       max_memory_restart: '4G',
       listen_timeout: 3000,
       kill_timeout: 8000,
-      combine_logs: true
+      combine_logs: true,
+      env: {
+        NODE_ENV: 'production',
+        PORT: '3000'
+      }
     }
   ]
 }
